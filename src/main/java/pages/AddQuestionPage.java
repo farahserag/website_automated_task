5638a9c09a518e5bf90e925c81b780d937b8644f
Farah Serag <farahserag2341@gmail.com>
2023-11-29 03:34:57 +0200

    Initial commit.

---
+++ src/main/java/pages/AddQuestionPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class QuestionPage {
+    private  WebDriver driver;
+    public QuestionPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+}
---
+++ src/test/java/navigate/NavigateTests.java
@@ -0,0 +1,2 @@
+package navigate;public class NavigateTests {
+}
